5f6c45516bfe5df74782c30e8b5407bc4880bf63
Christian Nuss <cnuss@users.noreply.github.com>
2024-05-03 12:18:43 +0000

    use stderr

--- handler.sh
+++ handler.sh
@@ -1,6 +1,7 @@
 function hello () {
-  echo "!!! hello from hello function"
-  env
+  echo "!!! hello from hello function" >&2
+  echo "!!! env:\n$(env)" >&2
+  echo "!!! event:\n$1" >&2
 
   RESPONSE="{\"body\": \"{\\\"message\\\": \\\"Hello Provided!\\\"}\", \"statusCode\": 200}"
 
