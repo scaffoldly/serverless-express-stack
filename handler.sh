a0a8099ac16e21975feb950f13330e47f2d909b8
Christian Nuss <cnuss@users.noreply.github.com>
2024-05-03 12:04:00 +0000

    log env

--- handler.sh
+++ handler.sh
@@ -1,4 +1,7 @@
 function hello () {
+  echo "!!! hello from hello function"
+  env
+
   RESPONSE="{\"body\": \"{\\\"message\\\": \\\"Hello Provided!\\\"}\", \"statusCode\": 200}"
 
   echo $RESPONSE
