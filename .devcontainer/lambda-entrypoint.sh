2e62b6b66381c74fb346521c621fc6dabfb85408
Christian Nuss <cnuss@users.noreply.github.com>
2024-05-28 06:25:56 +0000

    debug env a bit later

--- .devcontainer/lambda-entrypoint.sh
+++ .devcontainer/lambda-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-env
-
 if [ $# -ne 1 ]; then
   echo "entrypoint requires the handler name to be the first argument" 1>&2
   exit 142
 fi
 export _HANDLER="$1"
+
+env
 
 RUNTIME_ENTRYPOINT=/var/runtime/bootstrap
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
